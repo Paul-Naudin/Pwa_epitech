0b3f0e1c05c80ca1069231bfff5e0ead184a3472
Paul NAUDIN <paul.naudin@epitech.eu>
2024-01-16 15:15:07 +0100

    First commit of the new PWA using Vue

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: 'Bracket - Online Tournament Manager',
+        short_name: 'Bracket',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+      },
+    }),
+  ],
+})
